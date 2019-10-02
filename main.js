fc7e560e4c5d27d8dc4cd8dd3723cbb5af34cfff
Seth Painter <timytomy11@gmail.com>
2019-10-02 02:29:21 -0500

    switched to passing the data to the extension via messaging

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+chrome.tabs.executeScript({ file: 'input.js' });
+chrome.tabs.executeScript({ file: 'pageStasher.js' });
 document.querySelector("#btnNewStash").addEventListener("click", e => {
-	chrome.tabs.executeScript({file: 'input.js'});
-	chrome.tabs.executeScript({file: 'pageStasher.js'});
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => 
+		chrome.tabs.sendMessage(tabs[0].id, {action: "retrieveFormData"}, response => {
+			document.writeln(response.formData);
+		})
+	);
 });

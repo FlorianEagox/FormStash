652e66cf08e62620ad09752e3f71d4c2e268a8cb
Seth Painter <timytomy11@gmail.com>
2019-10-03 01:12:21 -0500

    links are displayed for all stashes

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// populate the popup with the existing stashes
+const stashList = document.querySelector("#stashList")
+chrome.storage.sync.get(null, stashes => Object.keys(stashes).forEach(stash => {
+	console.log("beep");
+	let li = document.createElement("li");
+	let a = document.createElement("a");
+	a.id = stash;
+	a.text = stash.split("|")[1];
+	a.href = "#";
+	a.class = "stashItem"
+	li.appendChild(a);
+	stashList.appendChild(li);
+}));
+
 // Inject our content scripts
 chrome.tabs.executeScript({ file: 'input.js' });
 chrome.tabs.executeScript({ file: 'pageStasher.js' });
@@ -6,14 +20,13 @@ document.querySelector("#btnNewStash").addEventListener("click", e => {
 		chrome.tabs.sendMessage(tabs[0].id, {action: "retrieveFormData"}, response => { // Tell the content script to collect all the inputs
 			 // For stashes un-named by user
 			let keyShortName;
-			let keyName;
+			let keyName = `${tabs[0].url}|`;
 			// go through the existing stashes and figure out how many we already have for this page
 			chrome.storage.sync.get(null, stashes => {
 				let stashIndex = Object.keys(stashes).filter(key => key.includes(tabs[0].url)).length;
 				keyShortName = `Stash ${stashIndex}` // TODO allow user to input stash name, for now
-				keyName = `${tabs[0].url}|${keyShortName}`;
+				keyName += keyShortName;
 				chrome.storage.sync.set({[keyName]: response});
 			});
-		})
-	);
+	}));
 });

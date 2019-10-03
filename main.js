25216fef730086348c3d27beee4bd02d81d3b58d
Seth Painter <timytomy11@gmail.com>
2019-10-03 00:30:36 -0500

    Cleaned up & commented. Fixed issue: index wouldn't incement so stashes would be overwritten.

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-class Stash {
-	constructor(id, data) {
-		this.id = id;
-		this.data = data;
-	}
-}
-
+// Inject our content scripts
 chrome.tabs.executeScript({ file: 'input.js' });
 chrome.tabs.executeScript({ file: 'pageStasher.js' });
 document.querySelector("#btnNewStash").addEventListener("click", e => {
-	chrome.tabs.query({active: true, currentWindow: true}, tabs =>
-		chrome.tabs.sendMessage(tabs[0].id, {action: "retrieveFormData"}, response => {
-			let stashIndex = 0;
-			chrome.storage.sync.get(null, stashes => Object.keys(stashes).forEach((key) =>
-				stashIndex += key.includes(tabs[0].url)
-			));
-			let keyShortName = `Stash ${stashIndex}`
-			let keyName = `${tabs[0].url}|${keyShortName}`;
-			chrome.storage.sync.set({[keyName]: response});
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => // get the current tab
+		chrome.tabs.sendMessage(tabs[0].id, {action: "retrieveFormData"}, response => { // Tell the content script to collect all the inputs
+			 // For stashes un-named by user
+			let keyShortName;
+			let keyName;
+			// go through the existing stashes and figure out how many we already have for this page
+			chrome.storage.sync.get(null, stashes => {
+				let stashIndex = Object.keys(stashes).filter(key => key.includes(tabs[0].url)).length;
+				keyShortName = `Stash ${stashIndex}` // TODO allow user to input stash name, for now
+				keyName = `${tabs[0].url}|${keyShortName}`;
+				chrome.storage.sync.set({[keyName]: response});
+			});
 		})
 	);
 });
--- pageStasher.js
+++ pageStasher.js
@@ -1,21 +1,21 @@
 chrome.runtime.onMessage.addListener(
 	(request, sender, sendResponse) => {
-		if (request.action == "retrieveFormData") {
-			const inputs = [];
-			const unsupportedInputTypes = ["submit", "button", "image", "file", "hidden", "password", "reset"]; //All the inputs our extension will ignore
-			const querySelection = "input:not([type=" + unsupportedInputTypes.join("]):not([type=") + "]), textarea";
-			const nodes = Array.prototype.slice.call(document.querySelectorAll("*")); // create an array of all dom elements
+		switch (request.action) {
+			case "retrieveFormData":
+				const inputs = []; // The form inputs we collect
+				const unsupportedInputTypes = ["submit", "button", "image", "file", "hidden", "password", "reset"]; //All the inputs our extension will ignore
+				const querySelection = "input:not([type=" + unsupportedInputTypes.join("]):not([type=") + "]), textarea";
+				const nodes = Array.prototype.slice.call(document.querySelectorAll("*")); // create an array of all dom elements
 
-			document.querySelectorAll(querySelection).forEach(el => { // get all the input elements of supported type
-				if (el.value != "") // don't mind empty inputs
-					if (el.id != "") // If we can, we want to store reference to teh ID rather than node index, but not all websites ID their inputs :/
-						inputs.push(new Input(el.id, el.value));
-					else
-						inputs.push(new Input(nodes.indexOf(el), el.value)); // get the index of the input in the array of all elements (please forgive me ^n^)
-			});
-			delete nodes;
-			//For now return as array
-			sendResponse(inputs);
+				document.querySelectorAll(querySelection).forEach(el => { // get all the input elements of supported type
+					if (el.value) // don't mind empty inputs
+						if (el.id && document.querySelectorAll(el.id).length == 1) // If we can, we want to store reference to the ID rather than node index, but not all websites ID their inputs and some have duplicate IDs :/
+							inputs.push([el.id, el.value]);
+						else
+							inputs.push([nodes.indexOf(el), el.value]); // get the index of the input in the array of all elements (please forgive me ^n^)
+						});
+				delete nodes;
+				sendResponse(inputs);
 		}
 	}
 );
